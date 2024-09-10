9ecd4cfd2dab0c604782cbc70e14e00647068e42
michelle-callejas <michellecruiz05@gmail.com>
2024-09-10 08:15:07 -0600

    prueba

---
+++ app/Controllers/solicitarMaterialesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class solicitarMaterialesController extends BaseController
+{
+    public function solicitarMateriales()
+    {
+        return view('solicitar_materiales');
+    }
+}
+
+
