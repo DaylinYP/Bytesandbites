c56f14ccd165aa9b44ed788c0aa03d268966d9da
michelle-callejas <michellecruiz05@gmail.com>
2024-09-12 11:21:49 -0600

    form_tecnico

--- app/Views/solicitudMateriales.php
+++ app/Views/solicitudMateriales.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Orden de Servicio</title>
+    
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -14,8 +15,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quantico:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet">
-    <link rel="icon" href="<?= base_url('img/favicon.ico') ?>" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="<?=base_url('img/favicon.ico') ?>" type="image/x-icon">
+    <link rel="stylesheet" href="<?php echo base_url('css/css_form_material.css');?>">
+        
+    <!--
     <style>
         * {
             margin: 0;
@@ -184,15 +187,18 @@
             transition: all .2s ease-out;
         }
     </style>
-
+-->
 </head>
 
 <body>
+<?=base_url('css/css_form_material.css');?>
+
     <div class="wrapper">
         <aside id="sidebar">
             <div class="d-flex">
                 <button class="toggle-btn" type="button">
                     <i class="lni lni-grid-alt"></i>
+
                 </button>
                 <div class="sidebar-logo">
                     <a href="#" class="titulo">Bytes & Bits</a>
@@ -256,7 +262,7 @@
         </aside>
         <div class="main p-3">
             <main class="container">
-                <form action="#">
+                <form action="nombre">
                     <div class="row">
                         <div class="col-4">
                             <h1 class="titulo">
@@ -324,15 +330,15 @@
                         
                         <div class="col py-4">
                             <label for="txt_usuario" class="pb-3">Descripción:</label>
-                            <textarea name="" class="form-control" id=""></textarea>
+                            <textarea name="txt_usuario" class="form-control" id=""></textarea>
                         </div>
 
 
                     
                         <div class="row pt-4">
                             <div class="col">
-                                <label for="txt_usuario" class="pb-3">Observaciones:</label>
-                                <textarea name="" class="form-control" style="height: 150px;" id=""></textarea>
+                                <label for="txt_observacion" class="pb-3">Observaciones:</label>
+                                <textarea name="txt_observacion" class="form-control" style="height: 150px;" id=""></textarea>
                             </div>
                             
                         </div>
@@ -363,7 +369,9 @@
                                 <textarea name="txt_observaciones_orden" class="form-control" style="height: 150px;" id=""></textarea>
                             </div>
                             </section>  
-                     
+                            </form>
+
+</main>         
                        
         </div>
         <hr>
@@ -371,15 +379,15 @@
         
 
 
-        </form>
-
-        </main>
+        
     </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
-    <script src="script.js"></script>
+    <!--
+    <script src="<?=base_url('js/script.js')?>"></script>
+    -->
     <script>
         const hamBurger = document.querySelector(".toggle-btn");
 
