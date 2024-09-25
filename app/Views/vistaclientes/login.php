0f2b9d3fe4160820f29652a1a8e7ae2c1061b197
DaylinYP <daylin.yl@gmail.com>
2024-09-25 08:47:35 -0600

    Actualizado

--- app/Views/vistaclientes/login.php
+++ app/Views/vistaclientes/login.php
@@ -4,14 +4,14 @@
 
 <div class="container">
     <br>
-    <h1 class="h1Bienvenidos text-center">SELECCIONE SU TIPO DE USUARIO</h1>
-    <hr>
+    <h1 class="h1Bienvenidos text-center ">SELECCIONE SU TIPO DE USUARIO</h1>
+    <hr class="usuario">
     <div class="row justify-content-center text-center btn-login">
-        <div class="col-12 col-md-6 mb-2  text-center">
-            <button type="button" class="btn btn-warning btn-lg btn-block">Cliente</button>
+        <div class="col-2 col-md-6 mb-2  text-center">
+            <button type="button" class="btn btn-primary btn-lg btn-block animate__animated animate__pulse animate__infinite animate__slow"><i class="bi bi-person-fill"> Cliente</i></button>
         </div>
         <div class="col-12 col-md-6 mb-2  text-center">
-            <button type="button" class="btn btn-danger btn-lg btn-block">Empleado</button>
+            <button type="button" class="btn btn-primary btn-lg btn-block animate__animated animate__pulse animate__infinite animate__slow"><i class="bi bi-person-vcard-fill"> Empleado</i></button>
         </div>
     </div>
 </div>
